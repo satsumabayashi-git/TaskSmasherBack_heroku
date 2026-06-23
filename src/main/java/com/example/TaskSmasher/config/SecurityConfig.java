f30e5a946c294e831e2ad7cadf43b67d8fc6b562
satsumabayashi-git <209392348+satsumabayashi-git@users.noreply.github.com>
2026-06-23 23:25:32 +0900

    PATCH:csrf設定変更

--- src/main/java/com/example/TaskSmasher/config/SecurityConfig.java
+++ src/main/java/com/example/TaskSmasher/config/SecurityConfig.java
@@ -88,9 +88,9 @@ public class SecurityConfig {
 						.deleteCookies("JSESSIONID"))
 				
 				// REST API 用に CSRF 無効
-//				.csrf((csrf) -> csrf
-//		                .ignoringRequestMatchers("/authentication","/logout")
-//		            )
+				.csrf((csrf) -> csrf
+		                .ignoringRequestMatchers("/authentication", "/todos", "/logout")
+		            )
 				;
 				
 				//CSRFを有効化
