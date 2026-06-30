91be3a9bc81032c47f28b613c52cc35feb421b70
satsumabayashi-git <209392348+satsumabayashi-git@users.noreply.github.com>
2026-06-30 19:24:03 +0900

    PATCH:cookie設定

--- src/main/java/com/example/TaskSmasher/config/SecurityConfig.java
+++ src/main/java/com/example/TaskSmasher/config/SecurityConfig.java
@@ -13,6 +13,7 @@ import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.csrf.CsrfFilter;
+import org.springframework.security.web.csrf.CsrfTokenRequestAttributeHandler;
 import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.CorsConfigurationSource;
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
@@ -43,8 +44,8 @@ public class SecurityConfig {
 	@Bean
 	public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
 
-//		CsrfTokenRequestAttributeHandler handler = new CsrfTokenRequestAttributeHandler();
-//		handler.setCsrfRequestAttributeName("_csrf");
+		CsrfTokenRequestAttributeHandler handler = new CsrfTokenRequestAttributeHandler();
+		handler.setCsrfRequestAttributeName("_csrf");
 
 		http
 				// ★HTTPリクエストに対するセキュリティ設定
