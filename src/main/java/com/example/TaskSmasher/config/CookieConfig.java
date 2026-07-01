7e35e17d6bf989284795b6d53c1c8041aa618ed4
satsumabayashi-git <209392348+satsumabayashi-git@users.noreply.github.com>
2026-07-01 22:58:33 +0900

    PATCH:cookie設定

--- src/main/java/com/example/TaskSmasher/config/CookieConfig.java
+++ src/main/java/com/example/TaskSmasher/config/CookieConfig.java
@@ -1,19 +1,17 @@
 package com.example.TaskSmasher.config;
 
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.session.web.http.DefaultCookieSerializer;
 
 @Configuration
 public class CookieConfig {
 
-    @Bean
-    public DefaultCookieSerializer cookieSerializer() {
-        DefaultCookieSerializer serializer = new DefaultCookieSerializer();
-//        serializer.setCookieName("_ctkn");
-        serializer.setSameSite("None"); 
-        serializer.setUseSecureCookie(true); 
-        
-        return serializer;
-    }
+//    @Bean
+//    public DefaultCookieSerializer cookieSerializer() {
+//        DefaultCookieSerializer serializer = new DefaultCookieSerializer();
+////        serializer.setCookieName("_ctkn");
+//        serializer.setSameSite("None"); 
+//        serializer.setUseSecureCookie(true); 
+//        
+//        return serializer;
+//    }
 }
