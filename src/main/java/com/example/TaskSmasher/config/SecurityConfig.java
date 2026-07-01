192a1db7b2517f8681a412023e10d3b2fbd988f8
satsumabayashi-git <209392348+satsumabayashi-git@users.noreply.github.com>
2026-07-02 01:51:22 +0900

    PATCH:csrf設定

--- src/main/java/com/example/TaskSmasher/config/SecurityConfig.java
+++ src/main/java/com/example/TaskSmasher/config/SecurityConfig.java
@@ -13,6 +13,7 @@ import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.csrf.CsrfFilter;
+import org.springframework.security.web.csrf.CsrfTokenRequestAttributeHandler;
 import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.CorsConfigurationSource;
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
@@ -93,7 +94,7 @@ public class SecurityConfig {
 		                .ignoringRequestMatchers("/authentication", "/logout", "todos/save")
 //		                .csrfTokenRepository(CookieCsrfTokenRepository())
 //		                .csrfTokenRepository(CookieCsrfTokenRepository.withHttpOnlyFalse())
-//		                .csrfTokenRequestHandler(new CsrfTokenRequestAttributeHandler()) 
+		                .csrfTokenRequestHandler(new CsrfTokenRequestAttributeHandler()) 
 		            )
 				
 				.addFilterAfter(new CsrfCookieFilter(), CsrfFilter.class)
